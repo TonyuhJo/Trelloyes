dc3f086267418d6ba3aa88a2f7292563df68d3e6
Tonya Stombaugh <tonya.stombaugh@gmail.com>
2019-10-16 12:23:53 -0700

    Create test files

---
+++ src/List.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import List from './List';
+
+describe('List component', () => {
+
+    const cards = [
+        {title: 'First card', content: 'lorem ipsum'},
+        {title: 'Second card', content: 'lorem ipsum'},
+        {title: 'Fifth card', content: 'lorem ipsum'}
+    ];
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<List key={1} cards={cards} header="Fourth list"></List>
+            , div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const tree = renderer
+            .create(<List key={1} header="Fourth list" cards={cards} />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
